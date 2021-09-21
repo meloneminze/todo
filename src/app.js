68f1ce8bb35c9bd68de8ddf771602ba3835a7691
Melanie Turek <melanie@derrabus.de>
2021-09-21 14:44:42 +0200

    Generating todos with default text

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
-import './index.scss';
-import { app } from './app';
+//selectors
 
-const elements = app();
-elements.forEach(element => {
-  document.body.appendChild(element);
-});
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//eventlistener
+
+todoButton.addEventListener('click', addTodo);
+
+//functions
+
+function addTodo(event){
+
+  event.preventDefault();
+
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hi';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+} 
